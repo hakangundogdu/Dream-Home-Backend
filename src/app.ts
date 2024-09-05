635de200fb2aa5bf2c9cc3a7c36b8c3b903e68fd
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2024-09-05 21:28:37 +0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -20,18 +20,15 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin!) || !origin) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
       }
     },
+    credentials: true,
   })
 );
-
-app.use("/api/", (req, res) => {
-  res.json({ message: "CORS enabled!" });
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
