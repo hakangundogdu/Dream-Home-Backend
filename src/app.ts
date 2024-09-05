6a0eb92da481bc623405b6517246c3f71e6ab51a
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2024-09-05 22:26:40 +0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,28 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  // 'https://dream-home-backend.vercel.app',
+  // Add any other frontend URLs here
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: false,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
